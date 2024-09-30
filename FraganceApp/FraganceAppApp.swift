738eaa5b5e0667ed9590e6db82e6a53eb8ba4342
Leo Mogiano <leomogiano@outlook.com>
2024-09-30 14:50:46 -0400

    Initial Commit

---
+++ FraganceApp/FraganceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FraganceAppApp.swift
+//  FraganceApp
+//
+//  Created by Leo Mogiano on 30/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FraganceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
